e882cee8896ecca1a1a03bd9e41673470b246611
LMutinda <lornamutinda3@gmail.com>
2024-01-11 12:24:05 +0300

    read

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege to user_0d_2 on database hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
