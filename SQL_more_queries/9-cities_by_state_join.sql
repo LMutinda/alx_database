08b0f598181e531e9c731f4ecc14e9750718bc07
LMutinda <lornamutinda3@gmail.com>
2024-01-11 12:47:51 +0300

    states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- List all cities with corresponding state names
 SELECT cities.id,
        cities.name,
-       CONCAT(states.name) AS state_name
+       CONCAT(states.name) 
 FROM cities, states
 WHERE states.id = cities.state_id
 ORDER BY cities.id ASC;
